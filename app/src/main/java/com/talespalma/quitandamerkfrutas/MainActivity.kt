43d8038578e68c8046320337b8b02ebd12183561
Tales Palma <tales.palma_2022@outlook.com>
2024-09-13 14:18:04 -0300

    Configuring project

---
+++ app/src/main/java/com/talespalma/quitandamerkfrutas/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.talespalma.quitandamerkfrutas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.talespalma.quitandamerkfrutas.ui.theme.QuitandaMerkFrutasTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuitandaMerkFrutasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Ola pessoal sera que deu certo ?",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QuitandaMerkFrutasTheme {
+        Greeting("Android")
+    }
+}
