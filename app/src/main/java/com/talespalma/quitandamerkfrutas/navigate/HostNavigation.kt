b4769540bfb1202db22345c097de0bfdab570eb7
Tales Palma <tales.palma_2022@outlook.com>
2024-09-15 16:47:35 -0300

    Implemente navigation Bar

--- app/src/main/java/com/talespalma/quitandamerkfrutas/navigate/HostNavigation.kt
+++ app/src/main/java/com/talespalma/quitandamerkfrutas/navigate/HostNavigation.kt
@@ -3,22 +3,32 @@ package com.talespalma.quitandamerkfrutas.navigate
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.talespalma.quitandamerkfrutas.ui.screens.HomeScreen
 
+
+sealed class Screens(val route: String, val icon: ImageVector) {
+    data object Home : Screens("Home", Icons.Filled.Home)
+    data object Add : Screens("Add", Icons.Filled.AddCircle)
+}
+
 @Composable
 fun HostNavigation(modifier: Modifier = Modifier, navController: NavHostController) {
     NavHost(navController = navController, startDestination = "home") {
-        composable("home") {
+        composable(Screens.Home.route) {
             HomeScreen()
         }
-        composable("2") {
+        composable(Screens.Add.route) {
             Column(
                 modifier.fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally,
--- app/src/main/java/com/talespalma/quitandamerkfrutas/ui/components/MenuBottom.kt
+++ app/src/main/java/com/talespalma/quitandamerkfrutas/ui/components/MenuBottom.kt
@@ -1,27 +1,47 @@
 package com.talespalma.quitandamerkfrutas.ui.components
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import com.talespalma.quitandamerkfrutas.navigate.Screens
 
+
+val listRoutes: List<Screens> = listOf(Screens.Home, Screens.Add)
 
 @Composable
-fun MenuBottom(modifier: Modifier = Modifier,navController: NavController?) {
-    BottomAppBar(containerColor = Color.Black){
-            IconButton(onClick = { navController?.navigate("home") }) {
-                Icon(imageVector = Icons.Filled.Home, contentDescription = "Home", tint = Color.White)
-            }
-            IconButton(onClick = { navController?.navigate("2") }) {
-                Icon(imageVector = Icons.Filled.AddCircle, contentDescription = "Home",tint = Color.White)
-            }
+fun MenuBottom(modifier: Modifier = Modifier, navController: NavController?) {
+
+
+    var selectedItem by remember { mutableIntStateOf(0) }
+    NavigationBar(
+        contentColor = Color.White,
+        containerColor = Color.Black
+    ) {
+        listRoutes.forEachIndexed { index, item ->
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = item.icon,
+                        contentDescription = item.route
+                    )
+                },
+                label = { item.route },
+                selected = selectedItem == index,
+                onClick = {
+                    selectedItem = index
+                    navController?.navigate(item.route)
+                }
+            )
+        }
     }
 }
 
